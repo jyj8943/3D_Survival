4a83c4a46e1f1c4490ff8e65db3315454de1e396
jyj8943 <jyj8943@gmail.com>
2025-08-06 16:04:45 +0900

    1-4 플레이어 상태 및 UI

---
+++ Assets/Scripts/PlayerCondition.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCondition : MonoBehaviour
+{
+    public UICondition uiCondition;
+    
+    Condition health
+    {
+        get { return uiCondition.health; }
+    }
+
+    Condition hunger
+    {
+        get { return uiCondition.hunger; }
+    }
+
+    Condition stamina
+    {
+        get { return uiCondition.stamina; }
+    }
+
+    public float noHungerHealthDecay;
+    
+    private void Update()
+    {
+        hunger.Subtract(hunger.passiveValue * Time.deltaTime);
+        stamina.Add(stamina.passiveValue * Time.deltaTime);
+
+        if (hunger.curValue <= 0)
+        {
+            health.Subtract(noHungerHealthDecay * Time.deltaTime);
+        }
+
+        if (health.curValue <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(float amount)
+    {
+        health.Add(amount);
+    }
+
+    public void Eat(float amount)
+    {
+        hunger.Add(amount);
+    }
+    
+    public void Die()
+    {
+        Debug.Log("죽었다!");
+    }
+}
