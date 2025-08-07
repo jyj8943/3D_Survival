259dd3069f90c5a30c60cedb67462bdbffbc981f
jyj8943 <jyj8943@gmail.com>
2025-08-07 18:03:37 +0900

    1-17 발걸음 소리와 뮤직 존 생성

---
+++ Assets/Scripts/MusicZone.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicZone : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public float fadeTime;
+    public float maxVolume;
+    private float targetVolume;
+
+    private void Start()
+    {
+        targetVolume = 0f;
+        audioSource = GetComponent<AudioSource>();
+        audioSource.volume = targetVolume;
+        audioSource.Play();
+    }
+
+    private void Update()
+    {
+        if (!Mathf.Approximately(audioSource.volume, targetVolume))
+        {
+            audioSource.volume = Mathf.MoveTowards(audioSource.volume, targetVolume, fadeTime * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            targetVolume = maxVolume;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            targetVolume = 0;
+        }
+    }
+}
